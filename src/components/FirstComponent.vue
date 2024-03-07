3ecfadd076849736325ae73d2e2d7fcf46abb050
palomacha100 <palomacha100@gmail.com>
2024-03-07 18:29:58 -0300

    Permite apagar a mensagem do input

--- src/components/FirstComponent.vue
+++ src/components/FirstComponent.vue
@@ -1,10 +1,15 @@
 <script setup lang="ts">
   const message = defineModel('message', {default: "Hello World"})
+
+  function limpar() { 
+    message.value = ""
+  }
 </script>
 <template>
   <h1>Meu primeiro componente</h1>
   <div>
       Dizendo olá, para variar: {{ message }}
+      <button @click="limpar">apaga tudo!</button>
   </div>
   <input v-model="message" />
 </template>
