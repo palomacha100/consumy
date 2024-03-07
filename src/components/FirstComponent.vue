45322f3da8f7992709c7b1cc3aacb24b45796f4d
palomacha100 <palomacha100@gmail.com>
2024-03-07 17:50:32 -0300

    Cria componente Hello World

---
+++ src/components/FirstComponent.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+  const message = defineModel('message', {default: "Hello World"})
+</script>
+<template>
+  <h1>Meu primeiro componente</h1>
+  <div>
+      Dizendo olá, para variar: {{ message }}
+  </div>
+  <input v-model="message" />
+</template>
+
